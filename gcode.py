f53e92018fe69a66ea3f453c97cb562425946cae
root <1292124493@qq.com>
2017-01-28 18:53:08 +0800

    改用jinja2模板引擎生成C文件,支持多实例生成

--- gcode.py
+++ gcode.py
@@ -1,16 +1,132 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8-*-
-import os
 import json
+from jinja2 import Template
 
 CONF_FILE = "./fsm_conf.json"
 
 OBJ_FILE_DIR = "./"
-CONF_HEADER_FILE = "Audfsm_conf.h"
-CONF_SOURCE_FILE = "Audfsm_conf.c"
+OBJ_FILE_NAME_TEMP = "{}fsm_conf.{}"
 
-STRUCT_END = "NULL};" + str(os.linesep)
-NEW_LINE = str(os.linesep)
+HEADER_FILE_TEMP = '''#ifndef {{name.upper()}}_FSM_CONF_H
+#define {{name.upper()}}_FSM_CONF_H
+
+#include "fsm.h"
+
+typedef enum
+{
+    {% for event in events -%}
+    {{event}}
+    {%- if loop.first %}=0{% endif -%}
+    ,
+    {% endfor %}{{name.upper()}}_EVENT_MAX,
+}{{name.upper()}}_EVENT;
+
+TYPE_STATE_MGR *{{name.upper()}}_StateMachineCreate(void);
+
+#endif
+'''
+
+SOURCE_FILE_TEMP = '''#include <stdio.h>
+#include "{{name}}fsm_conf.h"
+
+{% for state in states %}
+static void {{state["name"]}}_EntryFunc(void);
+static void {{state["name"]}}_ExitFunc(void);
+{% endfor %}
+
+{% for actuator in actuators -%}
+static void {{actuator["actuator"][1]}}_{{actuator["actuator"][2]}}Action(void);
+{% endfor %}
+
+{% for actuator in actuators -%}
+TYPE_ACTUATOR {{actuator["actuator"][0]}} = {{'{'}}{{actuator["actuator"][2]}}, {{actuator["actuator"][1]}}_{{actuator["actuator"][2]}}Action, (TYPE_STATE *){{states_hash[actuator["actuator"][3]]}}U{{'}'}};
+{% endfor %}
+
+TYPE_ACTUATOR *{{name}}AllActors[] = { \\
+{% for actuator in actuators -%}
+&{{actuator["actuator"][0]}}, \\
+{% endfor %}NULL{{'}'}};
+
+{% for state in states %}
+TYPE_STATE *{{state["name"]}}_State_Childs[] = {{'{'}}
+{%- for child in state["childs"] -%}
+(TYPE_STATE *){{states_hash[child]}}U,
+{%- endfor %} NULL{{'}'}};
+TYPE_ACTUATOR *{{state["name"]}}_State_Actuators[] = {
+{%- for actuator in state["actuators"] -%}
+&{{ actuator }},
+{%- endfor -%}
+NULL{{'}'}};
+TYPE_STATE {{state["name"]}}_State = {{'{'}} \\
+(TYPE_STATE *){{states_hash[state["parent"]]}}U, \\
+{{state["name"]}}_EntryFunc, \\
+{{state["name"]}}_ExitFunc, \\
+(TYPE_STATE **){{state["name"]}}_State_Childs, \\
+(TYPE_ACTUATOR **){{state["name"]}}_State_Actuators, \\
+{{'}'}};
+{% endfor %}
+
+TYPE_STATE *{{ name }}AllStates[] = {
+{%- for state in states -%}
+&{{state["name"]}}_State,
+{%- endfor %}NULL{{'}'}};
+
+TYPE_STACK {{name}}Stack = {{'{{0}, 0};'}}
+
+TYPE_STATE_MGR {{name}}StateMgr = {{'{'}}&root_State,&STA_2D_State,&{{name}}Stack{{'}'}};
+
+TYPE_STATE_MGR *{{name}}_StateMachineCreate(void)
+{{'{'}}
+    TYPE_ACTUATOR **pAllActors = (TYPE_ACTUATOR **){{name}}AllActors;
+    TYPE_STATE **pAllStates = (TYPE_STATE **){{name}}AllStates;
+    TYPE_STATE **pChilds = NULL;
+    T32U TargetStateID = 0;
+
+    while(*pAllActors != NULL)
+    {{'{'}}
+        TargetStateID = (T32U)((*pAllActors)->TargetState)-1U;
+        ((*pAllActors)->TargetState) = {{name}}AllStates[TargetStateID];
+        pAllActors++;
+    {{'}'}}
+
+    while(*pAllStates != NULL)
+    {{'{'}}
+        TargetStateID = (T32U)((*pAllStates)->pParent)-1U;
+        (*pAllStates)->pParent = {{name}}AllStates[TargetStateID];
+        pChilds = (*pAllStates)->pChilds;
+        while(*pChilds != NULL)
+        {{'{'}}
+            TargetStateID = (T32U)(*pChilds)-1U;
+            (*pChilds) = {{name}}AllStates[TargetStateID];
+            pChilds++;
+        {{'}'}}
+        pAllStates++;
+    {{'}'}}
+
+    return &{{name}}StateMgr;
+{{'}'}}
+
+{% for state in states%}
+static void {{state["name"]}}_EntryFunc(void)
+{
+	pprintf("{{state["name"]}}_EntryFunc\\n");
+}
+
+static void {{state["name"]}}_ExitFunc(void)
+{
+    pprintf("{{state["name"]}}_ExitFunc\\n");
+}
+{% endfor %}
+
+{% for actuator in actuators%}
+static void {{actuator["actuator"][1]}}_{{actuator["actuator"][2]}}Action(void)
+{
+    pprintf("{{actuator["actuator"][1]}}_{{actuator["actuator"][2]}}Action\\n");
+}
+{% endfor %}
+
+'''
 
 
 def print_iterable(items):
@@ -24,37 +140,19 @@ class ConfParser(object):
         with open(filename, "r") as f:
             self.conf = json.load(f)
 
-        self.actuators = self.conf.get("actuators", [])
+        self.name = self.conf.get("name", "")
         self.states = self.conf.get("states", [])
+        self.actuators = self.conf.get("actuators", [])
+        self.events = set()
+        self.states_hash = dict()
 
-    @staticmethod
-    def _get_states_func_declare(state):
-        return "static void {entryfunc}(void);{new_line}static void {exitfunc}(void);{new_line}{new_line}".format(
-            new_line=NEW_LINE,
-            **state)
-
-    @staticmethod
-    def _get_states_func_define(state):
-        return "static void {entryfunc}(void){new_line}{{{new_line}\tpprintf(\"{entryfunc}\\n\");{new_line}}}{new_line}{new_line}static void {exitfunc}(void){new_line}{{{new_line}\tpprintf(\"{exitfunc}\\n\");{new_line}}}{new_line}{new_line}".format(
-            new_line=NEW_LINE,
-            **state)
-
-    @staticmethod
-    def _get_actuators_func_declare(actuator):
-        return "static void {}(void);{new_line}".format(actuator["actuator"][3], new_line=NEW_LINE)
-
-    @staticmethod
-    def _get_actuators_func_define(actuator):
-        return "static void {0[3]}(void){new_line}{{{new_line}\tpprintf(\"{0[3]}\\n\");{new_line}}}{new_line}{new_line}".format(
-            actuator["actuator"],
-            new_line=NEW_LINE)
+        self.header_file_temp = Template(HEADER_FILE_TEMP)
+        self.source_file_temp = Template(SOURCE_FILE_TEMP)
 
     def _update_states(self):
         self.states.insert(0, dict(name="root", parent="root"))  # Insert the "root" node to the head
         temp_dict = {}
         for state in self.states:
-            state["entryfunc"] = state["name"] + "_EntryFunc"
-            state["exitfunc"] = state["name"] + "_ExitFunc"
             state["childs"] = []
             state["actuators"] = []
             temp_dict[state["name"]] = state
@@ -63,158 +161,42 @@ class ConfParser(object):
             temp_dict[state["parent"]]["childs"].append(state["name"])
 
         temp_dict["root"]["childs"].remove("root")
-
         self.states_index = temp_dict
-        self.states_list = list(self.states_index.keys())
+
+        for index, state in enumerate(self.states, 1):
+            self.states_hash[state["name"]] = index
 
     def _update_actuators(self):
         for actuator in self.actuators:
             act = actuator["actuator"]
             name = act[0] + "_" + act[1] + "Actor"
-            action = act[0] + "_" + act[1] + "Action"
+            self.events.add(act[1])
             act.insert(0, name)
-            act.insert(3, action)
             self._from_actuators_update_states(act)
+
+        self.events = list(self.events)
+        self.events.sort()
 
     def _from_actuators_update_states(self, act):
         self.states_index[act[1]]["actuators"].append(act[0])
 
     def resolve(self):
         self._update_states()
-        # print_iterable(self.states)
         self._update_actuators()
         print_iterable(self.actuators)
         print_iterable(self.states)
         print_iterable(self.states_index.items())
+        print_iterable(self.events)
+        print_iterable(self.states_hash.items())
 
     def _write_header_file(self):
-        f = open(CONF_HEADER_FILE, "w")
-        f.write("""#ifndef AUDFSM_CONF_H
-#define AUDFSM_CONF_H
-
-#include "fsm.h"
-
-typedef enum
-{
-    EVENT_1 = 0,
-    EVENT_2,
-    EVENT_3,
-    EVENT_4,
-    EVENT_5,
-    XXX_EVENT_MAX,
-}XXX_EVENT;
-""")
-        f.write("{new_line}TYPE_STATE_MGR *Aud_StateMachineCreate(void);{new_line}{new_line}".format(new_line=NEW_LINE))
-
-        f.write("""#endif
-
-""")
-        f.close()
-
-    def _format_actuator(self, actuator):
-        act = actuator["actuator"]
-        return "TYPE_ACTUATOR {0[0]} = {{{0[2]}, {0[3]}, (TYPE_STATE *){1}U}};{new_line}".format(
-            act, self.states_list.index(act[4])+1, new_line=NEW_LINE)
-
-    def _write_actuators(self, fd):
-        for actuator in self.actuators:
-            fd.write(self._format_actuator(actuator))
-
-        fd.write("{new_line}TYPE_ACTUATOR *AllActors[] = {{ \\{new_line}".format(new_line=NEW_LINE))
-        for actuator in self.actuators:
-            fd.write("&{},  \\{new_line}".format(actuator["actuator"][0], new_line=NEW_LINE))
-
-        fd.write(STRUCT_END)
-        fd.write(NEW_LINE)
-
-    def _writes_state_child(self, fd, state):
-        fd.write("TYPE_STATE *{name}_State_Childs[] = {{".format(**state))
-        for child in state["childs"]:
-            fd.write("(TYPE_STATE *){}U, ".format(self.states_list.index(child)+1))
-        fd.write(STRUCT_END)
-
-    def _writes_state_actuator(self, fd, state):
-        fd.write("TYPE_ACTUATOR *{name}_State_Actuators[] = {{".format(**state))
-        for actuator in state["actuators"]:
-            fd.write("&{}, ".format(actuator))
-        fd.write(STRUCT_END)
-
-    def _writes_states(self, fd):
-        for state in self.states:
-            self._writes_state_child(fd, state)
-            self._writes_state_actuator(fd, state)
-            fd.write(
-                "TYPE_STATE {0}_State = {{ \\{new_line}(TYPE_STATE *){1}U, \\{new_line}{0}_EntryFunc, \\{new_line}{0}_ExitFunc, \\{new_line}(TYPE_STATE **){0}_State_Childs, \\{new_line}(TYPE_ACTUATOR **){0}_State_Actuators, \\{new_line}}};{new_line}{new_line}".format(
-                    state["name"], self.states_list.index(state["parent"])+1, new_line=NEW_LINE))
-
-        fd.write("TYPE_STATE *AllStates[] = {")
-        for state in self.states_list:
-            fd.write("&{}_State, ".format(state))
-        fd.write(STRUCT_END)
-        fd.write(NEW_LINE)
-
-    def _write_data_const_func(self, fd):
-        fd.write("""TYPE_STACK Stack = {{0}, 0};
-
-TYPE_STATE_MGR StateMgr = {&root_State,&STA_2D_State,&Stack};
-
-TYPE_STATE_MGR *Aud_StateMachineCreate(void)
-{
-    TYPE_ACTUATOR **pAllActors = (TYPE_ACTUATOR **)AllActors;
-    TYPE_STATE **pAllStates = (TYPE_STATE **)AllStates;
-    TYPE_STATE **pChilds = NULL;
-    T32U TargetStateID = 0;
-
-    while(*pAllActors != NULL)
-    {
-        TargetStateID = (T32U)((*pAllActors)->TargetState)-1U;
-        ((*pAllActors)->TargetState) = AllStates[TargetStateID];
-        pAllActors++;
-    }
-
-    while(*pAllStates != NULL)
-    {
-        TargetStateID = (T32U)((*pAllStates)->pParent)-1U;
-        (*pAllStates)->pParent = AllStates[TargetStateID];
-        pChilds = (*pAllStates)->pChilds;
-        while(*pChilds != NULL)
-        {
-            TargetStateID = (T32U)(*pChilds)-1U;
-            (*pChilds) = AllStates[TargetStateID];
-            pChilds++;
-        }
-        pAllStates++;
-    }
-
-    return &StateMgr;
-}
-""")
-
-    def _write_func_declare(self, fd):
-        for state in self.states:
-            fd.write(ConfParser._get_states_func_declare(state))
-
-        for actuator in self.actuators:
-            fd.write(ConfParser._get_actuators_func_declare(actuator))
-
-        fd.write(NEW_LINE)
+        with open(OBJ_FILE_NAME_TEMP.format(self.name, "h"), "w") as f:
+            f.write(self.header_file_temp.render(name=self.name, events=self.events))
 
     def _write_source_file(self):
-        f = open(CONF_SOURCE_FILE, "w")
-        f.write(r'''#include <stdio.h>
-#include "Audfsm_conf.h"
-
-''')
-        self._write_func_declare(f)
-        self._write_actuators(f)
-        self._writes_states(f)
-        self._write_data_const_func(f)
-        for state in self.states:
-            f.write(ConfParser._get_states_func_define(state))
-
-        for actuator in self.actuators:
-            f.write(ConfParser._get_actuators_func_define(actuator))
-        f.close()
+        with open(OBJ_FILE_NAME_TEMP.format(self.name, "c"), "w") as f:
+            f.write(self.source_file_temp.render(name=self.name, states=self.states, actuators=self.actuators,
+                                                 states_hash=self.states_hash))
 
     def write_to_file(self):
         self._write_header_file()
